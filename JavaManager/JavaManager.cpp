83143dd1560e8b9adc92f63633dbd10bdec0f651
MMDDKK6500 <woobiewhy@gmail.com>
2025-02-27 18:32:28 -0300

    Start java now, but still need to do args

--- JavaManager/JavaManager.cpp
+++ JavaManager/JavaManager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 #include "filemanager.hpp"
 #include "config.hpp"
 #include "argparse/argparse.hpp"
@@ -9,6 +10,11 @@ using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
+
+	fs::path javaPath;
+
+	std::unique_ptr<Config> config;
+
 	argparse::ArgumentParser program("JavaManager");
 
 	program.add_argument("-jv", "--java-version")
@@ -30,18 +36,104 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	if (auto fn = program.present<int>("-jv")) {
-		cout << *fn;
-	}
-
 	if (auto fn = program.present("-c")) {
-		Config config(*fn);
+		config = std::make_unique<Config>(*fn);
 	}
-	else{
-		Config config();
+	else {
+		config = std::make_unique<Config>();
 	}
 
-	system("pause");
+	if (auto fn = program.present<int>("-jv")) {
+		//check if provided java version is installed
+		if (config->versionsInstalled.find(*fn) == config->versionsInstalled.end()) {
+			std::cerr << "Java version " << *fn << " is not installed" << std::endl;
+			return 1;
+		}
+	}
+
+	try {
+		auto javaArgs = program.get<std::vector<std::string>>("javaArgs");
+		std::cout << javaArgs.size() << " java arguments provided" << std::endl;
+		for (auto i = 0; i < javaArgs.size(); i++) {
+			if (i == 0) {
+				//check if first argument is a valid java executable from the bin folder of the java version
+				if (auto fn = program.present<int>("-jv")) {
+					javaPath = config->versionsInstalled[*fn];
+				}
+				else {
+					javaPath = config->versionsInstalled[config->defaultVersion];
+				}
+				
+
+				//TODO: cross-platform this
+				javaPath += "\\bin\\" + javaArgs[i] + ".exe";
+
+				//Check if the path exists
+				//cout << fs::current_path() << "\n";
+				if (!fs::exists(javaPath)) {
+					std::cerr << "Invalid java executable provided" << std::endl;
+					return 1;
+				}
+
+			}
+		}
+
+		
+		//use CreateProcessW to execute javaPath with javaArgs
+		std::cout << "Executing " << javaPath << std::endl;
+		
+		//prepare args
+		std:string javaExe = javaArgs[0] + ".exe";
+		std::wstring widestr = std::wstring(javaExe.begin(), javaExe.end());
+		const wchar_t* command = widestr.c_str();
+		//TODO: Implement the args
+
+		//
+		const wchar_t* workingDir = fs::current_path().c_str();
+
+
+		STARTUPINFO sia = { 0 };
+		sia.cb = sizeof(sia);
+		PROCESS_INFORMATION pi = { 0 };
+
+		// Create the child process
+		BOOL success = CreateProcessW(
+			javaPath.c_str(),  // Path to executable
+			const_cast<wchar_t*>(command),          // Command line arguments
+			NULL,                                   // Process attributes
+			NULL,                                   // Thread attributes
+			FALSE,                                  // Inherit handles
+			0,                                      // Creation flags
+			NULL,                                   // Environment
+			workingDir,                             // Working directory
+			&sia,                                   // Startup info
+			&pi);                                   // Process information
+
+		if (success)
+		{
+			// Wait for the process to exit
+			WaitForSingleObject(pi.hProcess, INFINITE);
+
+			// Process has exited - check its exit code
+			DWORD exitCode;
+			GetExitCodeProcess(pi.hProcess, &exitCode);
+
+			// At this point exitCode is set to the process' exit code
+
+			// Handles must be closed when they are no longer needed
+			CloseHandle(pi.hThread);
+			CloseHandle(pi.hProcess);
+		}
+		else
+		{
+			// CreateProcess failed
+			DWORD error = GetLastError();
+			std::cerr << "CreateProcess failed with error " << error << std::endl;
+		}
+	}
+	catch (std::logic_error& e) {
+		std::cout << "No java arguments provided" << std::endl;
+	}
 
 	return 0;
 }
